02735e57643682e97a0061245cf49a535854ef1a
alver <alver@MSI>
2022-11-08 22:17:15 +0800

    starproject

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alver
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        String a, e, i, o, u, huruf, vokal;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan 1 huruf dalam alphabet = ");
+        huruf=scan.next();
+        
+        if (huruf=a, e, i, o, u) {
+        
+    }
+        System.out.println("Hello World!");
+    }
+}
---
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alver
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Star Pattern Is ......");
+        System.out.println("-----------------");
+        for (int x=1; x<=3;x++) {
+            for (int y=1;y<=3;y++) {
+                if (x==3 || y==2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            } System.out.println(" ");
+        
+    }
+}
+}
+
+
+        
+        
+        
+     
+    
